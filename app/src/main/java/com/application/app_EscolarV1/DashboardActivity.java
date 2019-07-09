05f71a0cc3452189a236fc0c0860b96e35129f1c
Ricardo Torres <ricardo.torres@desysnet.com>
2019-07-08 19:50:13 -0500

    Login

---
+++ app/src/main/java/com/application/app_EscolarV1/DashboardActivity.java
@@ -0,0 +1,16 @@
+package com.application.app_EscolarV1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dashboard_layout);
+
+    }
+}
---
+++ app/src/main/java/com/application/app_EscolarV1/LoginActivity.java
@@ -0,0 +1,145 @@
+package com.application.app_EscolarV1;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+import ca.mimic.oauth2library.OAuth2Client;
+import ca.mimic.oauth2library.OAuthError;
+import ca.mimic.oauth2library.OAuthResponse;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class LoginActivity extends AppCompatActivity {
+
+    //Declaracion
+    EditText Lbl_UserId,Lbl_Password;
+    Button btn_Login;
+
+    final String url_Login = "https://desysnet.azure-api.net/v1/oauth/token";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_layout);
+
+        //
+        Lbl_UserId = (EditText) findViewById(R.id.Lbl_UserId);
+        Lbl_Password = (EditText) findViewById(R.id.Lbl_Password);
+        btn_Login = (Button) findViewById(R.id.btn_Login);
+
+        btn_Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+
+                String UserId = Lbl_UserId.getText().toString();
+                String password = Lbl_Password.getText().toString();
+
+                new LoginUser().execute(UserId, password);
+
+            }
+
+        });
+
+
+    }
+
+    public class LoginUser extends AsyncTask<String, Void, String>{
+        @Override
+        protected String doInBackground(String... strings){
+            String User = strings[0];
+            String Pass = strings[1];
+
+
+            OAuth2Client client = new OAuth2Client.Builder("","",url_Login)
+                    .grantType("password")
+                    .username(User)
+                    .password(Pass).build();
+
+            //OAuth2Client client = new OAuth2Client.Builder(""+User+"",""+Pass+"","","",  ""+url_Login+"").build();
+            OAuthResponse response = null;
+            try {
+                response = client.requestAccessToken();
+                if (response.isSuccessful()) {
+
+                    String accessToken = response.getAccessToken();
+
+                    String refreshToken = response.getRefreshToken();
+                    //abre dashboard
+                    Intent i = new Intent(LoginActivity.this, DashboardActivity.class);
+                    startActivity(i);
+                    finish();
+                }
+                else {
+
+                    OAuthError error = response.getOAuthError();
+
+                    String errorMsg = error.getError();
+
+                }
+                response.getCode();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+            /*
+            OkHttpClient okHttpClient = new OkHttpClient();
+            RequestBody formBody = new FormBody.Builder()
+                    .add("grant_type", "password")
+                    .add("Username", User)
+                    .add("Password", Pass)
+                    .build();
+
+
+            Request request = new Request.Builder()
+                    .url(url_Login)
+                    .post(formBody)
+                    .header("Content-Type", "application/x-www-form-urlencoded")
+                    .build();
+
+            Response response = null;
+
+            try{
+                response = okHttpClient.newCall(request).execute();
+                if(response.isSuccessful()){
+                    String result = response.body().string();
+                    if(result.equalsIgnoreCase("login")){
+                        //abre dashboard
+                        Intent i = new Intent(LoginActivity.this, DashboardActivity.class);
+                        startActivity(i);
+                        finish();
+                    }else{
+                        showToast("Email or Password mismatched!");
+                    }
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+            }*/
+            return null;
+        }
+    }
+
+    public void showToast(final String Text){
+        this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(LoginActivity.this,
+                        Text, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
